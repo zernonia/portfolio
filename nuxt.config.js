bdf1a38e80e9c193b780e98944164426190e8a82
zernonia <59365435+zernonia@users.noreply.github.com>
2020-03-15 01:17:27 +0800

    Added Blog dir and Blog

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,16 +55,16 @@ export default {
     extend (config, ctx) {
     }
   },
-  // generate: {
-  //   routes: function() {
-  //     const fs = require('fs');
-  //     const path = require('path');
-  //     return fs.readdirSync('./assets/content/blog').map(file => {
-  //       return {
-  //         route: `/blog/${path.parse(file).name}`, // Return the slug
-  //         payload: require(`./assets/content/blog/${file}`),
-  //       };
-  //     });
-  //   },
-  // },
+  generate: {
+    routes: function() {
+      const fs = require('fs');
+      const path = require('path');
+      return fs.readdirSync('./assets/content/blog').map(file => {
+        return {
+          route: `/blog/${path.parse(file).name}`, // Return the slug
+          payload: require(`./assets/content/blog/${file}`),
+        };
+      });
+    },
+  },
 }
---
+++ pages/blog/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-for="blog in blogPosts" :key="blog.title">
+      {{ blog.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts;
+    },
+  },
+};
+</script>
+
