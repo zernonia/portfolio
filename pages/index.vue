a438c87916cef26e18dc3598fc5df57741157e6b
zernonia <59365435+zernonia@users.noreply.github.com>
2020-03-16 01:23:00 +0800

    Using Tailwindcss to edit index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,20 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        portfolio
-      </h1>
-      <h2 class="subtitle">
-        My groovy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="container min-h-screen flex-row justify-center align-middle m-0 p-4 min-w-full sm:p-8 md:p-12 md:flex">
+      <div class=" flex justify-between content-center md:w-1/3 md:flex-row flex-wrap md:p-8">
+        <h1 class=" text-gray-900 font-extrabold tracking-wider mt-2 uppercase text-lg sm:text-2xl md:text-4xl leading-none">Hello World</h1>
+        <a href="/blog" class=" text-xs uppercase tracking-widest font-bold px-4 py-1 mt-2 bg-gray-800 text-white rounded-lg hover:bg-gray-600 sm:px-6 sm:py-2 self-center md:mt-4">Blog</a>
+      </div>
+      <div class=" md:w-2/3 mt-4"> 
+        <img src="~/static/img/zern.jpeg" alt="zern portfolio" class=" shadow-lg mt-2 rounded-lg object-cover">
+        <h3 class=" text-gray-700 mt-4 text-xs md:text-lg">First ever blog about Web Development, Data Science and more!</h3> 
       </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
   },
   head() {
     return {
@@ -49,34 +30,4 @@ export default {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
